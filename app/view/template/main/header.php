b2b2dc92887fe95f3b7dcd0d34babeca280fcec1
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-01-11 00:41:44 +0700

    update menu utama

--- app/view/template/main/header.php
+++ app/view/template/main/header.php
@@ -127,11 +127,6 @@
                                                         <small class="text-muted"><?= $_SESSION['auth']['email'] ?></small>
                                                     </div>
                                                 </div>
-                                                <?php if (!route_has('antrian')) : ?>
-                                                    <a class="dropdown-item" href="<?= route('antrian') ?>">
-                                                        <i class="dropdown-icon fe fe-file"></i> Antrian Pasien
-                                                    </a>
-                                                <?php endif ?>
                                                 <?php if (!route_has('profile')) : ?>
                                                     <a class="dropdown-item" href="<?= route('profile', ['cr' => get('r')]) ?>">
                                                         <i class="dropdown-icon fe fe-user"></i> Profil
@@ -159,97 +154,89 @@
             <!-- /app-Header -->
 
             <!--APP-SIDEBAR-->
-            <?php if (false) : ?>
-                <div class="sticky">
-                    <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
-                    <div class="app-sidebar">
-                        <div class="side-header">
-                            <a class="header-brand1" href="<?= base_url() ?>">
-                                <!-- {{-- light landscape --}}
-                            <img src="{{ asset(settings()->get(set_admin('app.foto_light_landscape_mode'))) }}" class="header-brand-img desktop-logo" alt="logo">
 
-                            {{-- light --}}
-                            <img src="{{ asset(settings()->get(set_admin('app.foto_light_mode'))) }}" class="header-brand-img toggle-logo" alt="logo">
+            <div class="sticky">
+                <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
+                <div class="app-sidebar">
+                    <div class="side-header">
+                        <a class="header-brand1" href="<?= base_url() ?>">
+                            <img src="<?= asset('assets/template/admin/logo.png') ?>" altSrc="<?= asset('assets/template/admin/logo.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img light-logo1" alt="logo">
+                            <img src="<?= asset('assets/template/admin/logo-grid.png') ?>" altSrc="<?= asset('assets/template/admin/logo-grid.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img light-logo" alt="logo">
+                            <img src="<?= asset('assets/template/admin/logo.png') ?>" altSrc="<?= asset('assets/template/admin/logo.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img desktop-logo" alt="logo">
+                            <img src="<?= asset('assets/template/admin/logo-grid.png') ?>" altSrc="<?= asset('assets/template/admin/logo-grid.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img toggle-logo" alt="logo">
+                        </a>
+                        <!-- LOGO -->
+                    </div>
+                    <div class="main-sidemenu">
+                        <div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z" />
+                            </svg></div>
+                        <ul class="side-menu">
 
-                            {{-- dark --}}
-                            <img src="{{ asset(settings()->get(set_admin('app.foto_dark_mode'))) }}" class="header-brand-img light-logo" alt="logo">
+                            <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('', 'active') ?>" data-bs-toggle="slide" href="<?= base_url() ?>">
+                                    <i class="side-menu__icon fas fa-home"></i>
+                                    <span class="side-menu__label">Home</span>
+                                </a>
+                            </li>
 
-                            {{-- dark landscape --}}
-                            <img src="{{ asset(settings()->get(set_admin('app.foto_dark_landscape_mode'))) }}" class="header-brand-img light-logo1" alt="logo"> -->
+                            <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('kecamatan', 'active') ?>" data-bs-toggle="slide" href="<?= route('kecamatan') ?>">
+                                    <i class="side-menu__icon fas fa-clipboard-list"></i>
+                                    <span class="side-menu__label">Kecamatan</span>
+                                </a>
+                            </li>
 
-                                <img src="<?= asset($setting['logo_white_landscape']) ?>" altSrc="<?= asset('assets/template/admin/logo.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img light-logo1" alt="logo">
-                                <img src="<?= asset($setting['logo_white']) ?>" altSrc="<?= asset('assets/template/admin/logo-grid.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img light-logo" alt="logo">
-                                <img src="<?= asset($setting['logo_landscape']) ?>" altSrc="<?= asset('assets/template/admin/logo.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img desktop-logo" alt="logo">
-                                <img src="<?= asset($setting['logo']) ?>" altSrc="<?= asset('assets/template/admin/logo-grid.png') ?>" onerror="this.src = $(this).attr('altSrc')" style="max-height: 53px;" class="header-brand-img toggle-logo" alt="logo">
-                            </a>
-                            <!-- LOGO -->
-                        </div>
-                        <div class="main-sidemenu">
-                            <div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z" />
-                                </svg></div>
-                            <ul class="side-menu">
-
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('', 'active') ?>" data-bs-toggle="slide" href="<?= base_url() ?>">
-                                        <i class="side-menu__icon fas fa-home"></i>
-                                        <span class="side-menu__label">Dashboard</span>
-                                    </a>
-                                </li>
-
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('registrasi', 'active') ?>" data-bs-toggle="slide" href="<?= route('registrasi') ?>">
-                                        <i class="side-menu__icon fas fa-edit"></i>
-                                        <span class="side-menu__label">Registrasi</span>
-                                    </a>
-                                </li>
-
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('pasien', 'active') ?>" data-bs-toggle="slide" href="<?= route('pasien') ?>">
-                                        <i class="side-menu__icon fas fa-user"></i>
-                                        <span class="side-menu__label">Pasien</span>
-                                    </a>
-                                </li>
-
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('antrian', 'active') ?>" data-bs-toggle="slide" href="<?= route('antrian') ?>">
-                                        <i class="side-menu__icon fas fa-clipboard-list"></i>
-                                        <span class="side-menu__label">Antrian</span>
-                                    </a>
-                                </li>
-
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('display', 'active') ?>" data-bs-toggle="slide" href="<?= route('display') ?>">
-                                        <i class="side-menu__icon fas fa-tv"></i>
-                                        <span class="side-menu__label">Display Antrian</span>
-                                    </a>
-                                </li>
+                            <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('calon', 'active') ?>" data-bs-toggle="slide" href="<?= route('calon') ?>">
+                                    <i class="side-menu__icon fas fa-user"></i>
+                                    <span class="side-menu__label">Calon</span>
+                                </a>
+                            </li>
 
 
-                                <li class="slide">
-                                    <a class="side-menu__item <?= route_active_class('catat_rm', 'active') ?>" data-bs-toggle="slide" href="<?= route('catat_rm') ?>">
-                                        <i class="side-menu__icon fas fa-pencil-alt"></i>
-                                        <span class="side-menu__label">Catat RM Pasien</span>
-                                    </a>
-                                </li>
 
-                                <li class="slide">
-                                    <a class="side-menu__item" data-bs-toggle="slide" href="<?= route('login.logout') ?>">
-                                        <i class="side-menu__icon fas fa-sign-out-alt"></i>
-                                        <span class="side-menu__label">Keluar</span>
-                                    </a>
-                                </li>
-                            </ul>
-                            <div class="slide-right" id="slide-right">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z" />
-                                </svg>
-                            </div>
+
+                            <!-- <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('registrasi', 'active') ?>" data-bs-toggle="slide" href="<?= route('registrasi') ?>">
+                                    <i class="side-menu__icon fas fa-edit"></i>
+                                    <span class="side-menu__label">Registrasi</span>
+                                </a>
+                            </li>
+
+
+
+                            <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('display', 'active') ?>" data-bs-toggle="slide" href="<?= route('display') ?>">
+                                    <i class="side-menu__icon fas fa-tv"></i>
+                                    <span class="side-menu__label">Display Antrian</span>
+                                </a>
+                            </li>
+
+
+                            <li class="slide">
+                                <a class="side-menu__item <?= route_active_class('catat_rm', 'active') ?>" data-bs-toggle="slide" href="<?= route('catat_rm') ?>">
+                                    <i class="side-menu__icon fas fa-pencil-alt"></i>
+                                    <span class="side-menu__label">Catat RM Pasien</span>
+                                </a>
+                            </li> -->
+
+                            <li class="slide">
+                                <a class="side-menu__item" data-bs-toggle="slide" href="<?= route('login.logout') ?>">
+                                    <i class="side-menu__icon fas fa-sign-out-alt"></i>
+                                    <span class="side-menu__label">Keluar</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="slide-right" id="slide-right">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z" />
+                            </svg>
                         </div>
                     </div>
-                    <!--/APP-SIDEBAR-->
                 </div>
-            <?php endif ?>
+                <!--/APP-SIDEBAR-->
+            </div>
 
             <!--app-content open-->
             <div class="main-content mt-0 hor-content">
